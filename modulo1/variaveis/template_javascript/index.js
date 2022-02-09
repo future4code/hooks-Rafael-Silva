f59ed95e66a90d5d9384065f15be3defd076e5f9
User <rafael.silva-2014@outlook.com>
2022-02-08 23:16:58 -0300

    Meus exercicios do VSCode

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,51 @@
+
+//let nome = prompt ("Qual é seu nome?") 
+
+//let idade = prompt ("Qual é sua idade?")
+
+//console.log(typeof nome, typeof idade)
+// Resultado foi Strigs Strigs
+
+//console.log = ("Seu nome é",nome,"Sua idade é",idade,"anos")
+
+ //let a = 10
+ //let b = 10
+ //console.log(b)
+ //b= 5
+ //console.log(a,b)
+ // A variavel a e a b serão impressos 
+
+//let a = 10
+//let b = 20
+//c = a
+///b = c
+//a = b
+
+//console.log(a, b, c)
+ // oque sera impresso no console é as variaveis a,b,c
+
+ //let hora = prompt("Quantas horas você trabalha por dia?")
+ //let salario = prompt("Quanto você /recebe por dia?")
+//alert(`Voce recebe ${hora/salario} por hora`)
+
+ //let nome = prompt ("Seu nome?")
+ //let idade = prompt ("Sua idade?")
+ //console.log (typeof nome, typeof /////idade)
+ //console.log ("Seu nome é",nome,"Sua idade é",idade)
+ // Resultado foi Strip Strip
+ // porque as variaveis que declarei foram apenas let 
+
+ //let =prompt ("Você esta usando uma roupa azul hoje?")
+
+ //console.log = prompt ("Sua Roupa é azul - SIM ",)
+// Aqui iremos trocar os valores
+//let a = 10
+//let b = 25
+//c
+//a = c
+//b = a
+//b = a
+ //Agora  teremos os resultados da troca 
+ //console.log("O novo valor de a é",b) // O novo valor de a é 25
+//console.log("O novo valor de b é", a) // O novo valor de b é 10
+//console.log("O valor de c virou a")
