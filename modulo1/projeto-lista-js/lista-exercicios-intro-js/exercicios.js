747a8beb10a2d2d12b581614e930945770caf4b7
User <rafael.silva-2014@outlook.com>
2022-04-10 16:10:54 -0300

    Lista-de-Java-Script-Feita

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -1,94 +1,152 @@
 // EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
 
 // EXERCÍCIO 0A
-function soma(num1, num2) { // Um exemplo Já foi resolvido antes
-  
-  return num1 + num2 //  Exemplo já foi resolvido  antes
+function soma(num1, num2) {
+  // implemente sua lógica aqui
+  return num1 + num2
 }
 
 // EXERCÍCIO 0B
 function imprimeMensagem() {
-  
+  // implemente sua lógica aqui
   const mensagem = prompt('Digite uma mensagem!')
 
-  console.log(mensagem) //Esse exemplo não respondi porque ja tava resolvido antes
+  console.log(mensagem)
 }
 
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
-  // implemente sua lógica aqui
 
+   let alTura = Number(prompt("Responda qual seria a sua altura"))
+   let larGuradaárea = Number(prompt("Qual é a sua  largura atualmente?"))
+   let noVaLargura=alTura*larGuradaárea
+
+   console.log(noVaLargura)
 }
+
 
 // EXERCÍCIO 02
 function imprimeIdade() {
-  // implemente sua lógica aqui
+  let idadeNumero = Number(prompt("qual é o ano atual?"))
 
+  let anoNumero = Number(prompt("Qual é o ano de seu nascimento?"))
+
+  let novoAno= idade-anoNumero
+
+  console.log(novoAno)
+  
 }
+
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  return peso /(altura*altura)
-  // Retonar o valor altura multiplicada pela mesma altura
+   let recebe = peso
+   let recebe2 = altura
 
+   let metrosNumber = recebe2*recebe2
+   let pesoMultiplicado = recebe
+
+   return pesoMultiplicado /metrosNumber
 
 }
 
 // EXERCÍCIO 04
-function imprimeInformacoesUsuario() {
-  // implemente sua lógica aqui
-  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+function imprimeInformacoesUsuario(){
+  let pergunta= (prompt("Escreva aqui qual é o  seu nome?"))
+
+  let anos= (prompt(" Escreva agora qual é a sua idade atualmente?"))
+
+  let email= (prompt("Qual é o seu email?"))
+  
+ 
+  console.log(`Meu nome é ${pergunta}, tenho ${anos} anos, e o meu email é ${email}.`)
 
 }
 
+
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
-  // implemente sua lógica aqui
+const questionCollor= (prompt("Escreva aqui  uma cor  favorita"))
+
+const questionCollor2= (prompt("Fale a  segunda cor favorita"))
+
+const questionCollor3= (prompt("Fale  a terceira  cor favorita"))
+
+const arrayNovoCor=[questionCollor,questionCollor2,questionCollor3]
+ 
+ console.log(arrayNovoCor)
 
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
-  // implemente sua lógica aqui
+  
+  let fraseIntrig =string.toUpperCase()
 
+  return fraseIntrig
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
+  let ingresso= custo
 
+  let custoTotal= valorIngresso
+
+  let ingressoValor= ingresso/custoTotal 
+
+ return ingressoValor
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
-  // implemente sua lógica aqui
+  
 
+  let booleano= string1>string2
+  
+  
+return booleano
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
-  // implemente sua lógica aqui
+ 
+  return array [0]
+  
+  
 
 }
 
 // EXERCÍCIO 10
-function retornaUltimoElemento(array) {
-  // implemente sua lógica aqui
+function retornaUltimoElemento(array)
+{ 
+  let arrayNovo= array[array.length-1]
 
+return arrayNovo
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
-  // implemente sua lógica aqui
+  
+ let valorArray=array[0]
+ let outroArray = array[array.length-1]
+ array[0]= outroArray
 
+ array[array.length-1] = valorArray
+
+  return array
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
-  // implemente sua lógica aqui
 
+ let novoValorBooleano= string1//variavel recebe uma string
+
+ let valorBooleano= string2// recebe outra string
+
+return novoValorBooleano.toUpperCase()===valorBooleano.toUpperCase()
+// comparação das duas strings atribuidas na variavel 
 }
 
 // EXERCÍCIO 13
