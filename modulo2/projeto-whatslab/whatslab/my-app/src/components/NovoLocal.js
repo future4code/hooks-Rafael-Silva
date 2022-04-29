99962039e19624e4854a1385dcff7b67878ef623
User <rafael.silva-2014@outlook.com>
2022-04-29 17:23:21 -0300

    ProjetoWhatsLab

---
+++ modulo2/projeto-whatslab/whatslab/my-app/src/components/NovoLocal.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Conta = styled.div`
+  width:550px;
+  height:500px;
+  display:flex;
+  border: 1px solid black;
+  margin:auto;
+  flex-direction:column;
+`
+export const Header = styled.div`
+  color: black;
+`
+
+export const NovaM = styled.div`
+  flex-grow:1;
+`
+
+export const Footer = styled.div`
+  display:flex;
+`
+
+export const Mensagem = styled.div`
+  
+`
