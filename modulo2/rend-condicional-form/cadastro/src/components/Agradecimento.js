70c9c484dd9bdcbf1d08e4ce4b972d986984d08d
User <rafael.silva-2014@outlook.com>
2022-05-03 22:41:17 -0300

    ReactFeito

---
+++ modulo2/rend-condicional-form/cadastro/src/components/Agradecimento.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+class Agradecimento extends React.Component {
+    render() {
+      return(
+      <div> 
+          <h1> O FORMULÁRIO ACABOU</h1>
+          <p>Muito obrigado por participar! Entraremos em contato!</p>
+     </div>
+
+
+      )
+
+ }
+
+
+}
+export default Agradecimento;
---
+++ modulo2/rend-condicional-form/cadastro/src/components/F2.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+
+class Etapa2 extends React.Component {
+    render() {
+      return(
+      <div> 
+          <h4> 2.INFORMAÇÕES DO ENSINO SUPERIOR</h4> 
+         <h4> 5.Qual curso?</h4>
+     <input type={this.F2}></input> 
+
+     <h4> .6. Qual a unidade de ensino?</h4>
+     <input type={this.F2}></input>
+     
+     </div>
+
+
+
+
+      )
+
+ }
+
+
+}
+export default Etapa2;
+
