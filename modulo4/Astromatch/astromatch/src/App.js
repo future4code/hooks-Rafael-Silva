72b011455246831defd2a75a0f2621e5a5039056
User <rafael.silva-2014@outlook.com>
2022-06-10 21:11:13 -0300

    Astromatch

---
+++ modulo4/Astromatch/astromatch/src/App.js
@@ -0,0 +1,17 @@
+import TelaInicial from "./components/TelaInicial";
+import TelaMatches from "./components/TelaMatches";
+
+const TelaInicial1 = () => {
+    return (
+      <div>
+       
+        <TelaInicial/>
+        <TelaMatches/>
+      </div>
+    );
+  };
+  
+
+
+
+export default TelaInicial1;
